fc6ac82a37de361f6ad3547d33d93ecef84de125
ProudOwl <solovlad59@mail.ru>
2025-04-25 20:23:15 +0700

    Update Task1.java

--- Task1/Task1.java
+++ Task1/Task1.java
@@ -18,7 +18,7 @@ public class Main {
         while (true) {
             if (scanner.hasNextInt()) {
                 N = scanner.nextInt();
-                if (N > 0) break;
+                if (N > 0 && N <= 26) break;
             } else {
                 scanner.next(); // Сбрасываем неверный ввод
             }
